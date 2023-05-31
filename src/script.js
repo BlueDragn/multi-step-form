cb0f6167f1e8fa5a8424a7f867a623303368bb68
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2023-05-31 17:38:19 +0530

    added Function to update the style of the list items based on the current section

--- src/script.js
+++ src/script.js
@@ -3,7 +3,19 @@
 var currentPageIndex = 0;
 var pages = document.getElementsByClassName('form-container');
 var thankYouSection = document.getElementById("thank_you");
+var stepListItems = document.getElementsByClassName('step-list');
 
+
+// Function to update the style of the list items based on the current section
+function updateStepList() {
+  // Reset the style of all list items
+  for (var i = 0; i < stepListItems.length; i++) {
+    stepListItems[i].classList.remove('active');
+  }
+
+  // Set the style for the current section
+  stepListItems[currentPageIndex].classList.add('active');
+}
 
 //Function to Navigate to different section of form
 function navigatePage(offset) {
@@ -14,9 +26,11 @@ function navigatePage(offset) {
   // Update the current page index
   currentPageIndex += offset;
   console.log(currentPageIndex);
+
   // Ensure the index stays within bounds
   if (currentPageIndex < 0) {
     currentPageIndex = 0;
+
   } else if (currentPageIndex >= pages.length) {
     currentPageIndex = pages.length - 1;
   }
@@ -25,10 +39,14 @@ function navigatePage(offset) {
 if (currentPageIndex < pages.length - 1) {
   pages[currentPageIndex].style.display = 'block';
   thankYouSection.style.display = 'none';
+
 } else {
   pages[currentPageIndex].style.display = 'none';
   thankYouSection.style.display = 'block';
 }
 
+  // Update the style of the list items
+  updateStepList();
 }
-
+// Call the updateStepList function initially to set the style for the first section
+updateStepList();
