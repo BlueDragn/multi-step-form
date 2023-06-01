0d1bac5da8da17c1cde2b39a4fc5fa6b097dd0eb
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2023-06-01 11:42:55 +0530

    fixed confirm button issue

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-
 //Select the elements
 var currentPageIndex = 0;
 
@@ -7,8 +6,11 @@ var pages = document.getElementsByClassName('form-container');
 var thankYouSection = document.getElementById("thank_you");
 var stepListItems = document.getElementsByClassName('step-list');
 var nextBtn = document.getElementById("next_btn");
+var backBtn = document.getElementById("back_btn")
 var confirmBtn = document.getElementById("confirm_btn");
 // Function to update the style of the list items based on the current section
+
+
 function updateStepList() {
   // Reset the style of all list items
   for (var i = 0; i < stepListItems.length; i++) {
@@ -23,21 +25,32 @@ function updateStepList() {
 function navigatePage(offset) {
 
   // Hide the current page
-    pages[currentPageIndex].style.display = 'none';
- // Update the current page index
+  pages[currentPageIndex].style.display = 'none';
+  // Update the current page index
   currentPageIndex += offset;
 
   //Ensure the index stays within bounds
   if (currentPageIndex < 0) {
     currentPageIndex = 0;
-    console.log(currentPageIndex);
-  } else if (currentPageIndex >= pages.length-1) {
-    currentPageIndex = pages.length -1;
-    nextBtn.style.display = 'none';
-    confirmBtn.style.display = 'block';
+
+
+  } else if (currentPageIndex >= pages.length - 1) {
+    currentPageIndex = pages.length - 1;
+    //nextBtn.style.display = 'none';
+    //confirmBtn.style.display = 'block';
   }
   // Show the new current page
   pages[currentPageIndex].style.display = 'block';
+
+
+   // Hide or show the next and confirm buttons
+   if (currentPageIndex === pages.length - 1) {
+    nextBtn.style.display = 'none';
+    confirmBtn.style.display = 'block';
+  } else {
+    nextBtn.style.display = 'block';
+    confirmBtn.style.display = 'none';
+  }
   // Update the style of the list items
   updateStepList();
 }
@@ -49,6 +62,9 @@ function confirmPage() {
 
   // Show the thank you section
   thankYouSection.style.display = "block";
+  confirmBtn.style.display = 'none';
+  backBtn.style.display = 'none';
+
 }
 
 // Call the updateStepList function initially to set the style for the first section
